d8f4eb702f0525479ab6367bd5f93ca1e39fcaa1
albankerloch <alban.kerloch@gmail.com>
2021-10-13 23:24:39 +0200

    save pair

--- Algo.hpp
+++ Algo.hpp
@@ -51,6 +51,82 @@ namespace ft
 		}
     };
 
+	template <class T1, class T2>
+	struct pair
+	{
+		public :
+
+		typedef T1 	first_type;
+        typedef T2 	second_type;
+		first_type	first;
+		second_type	second;
+
+		pair()
+		{
+			this->first = first();
+            this->second = second();
+        }
+
+		pair(const T1& x, const T2& y)
+		{
+			this->first = x;
+			this->second = y;
+		};
+
+		template< class U1, class U2 >
+		pair( const pair<U1, U2>& p )
+		{
+			this->first = p.first;
+			this->second = p.second;
+		};
+
+		pair& operator= (const pair& p)
+		{
+			if (*this == p)
+				return (*this);
+			this->first = p.first;
+			this->second = p.second;
+			return (*this);
+		}
+	};
+
+	template <class T1, class T2>
+	bool operator!= (const ft::pair<T1,T2>& lhs, const ft::pair<T1,T2>& rhs)
+	{
+		return !(lhs == rhs);
+	};
+    
+    template <class T1, class T2>
+	bool operator<  (const ft::pair<T1,T2>& lhs, const ft::pair<T1,T2>& rhs)
+	{
+		return (lhs.first < rhs.first || (!(rhs.first < lhs.first) && lhs.second < rhs.second));
+	};
+
+    template <class T1, class T2>
+	bool operator<= (const ft::pair<T1,T2>& lhs, const ft::pair<T1,T2>& rhs)
+	{
+		return !(rhs < lhs);
+	};
+
+    template <class T1, class T2>
+	bool operator>  (const ft::pair<T1,T2>& lhs, const ft::pair<T1,T2>& rhs)
+	{
+		return (rhs < lhs);
+	};
+
+    template <class T1, class T2>
+	bool operator>= (const ft::pair<T1,T2>& lhs, const ft::pair<T1,T2>& rhs)
+	{
+		return !(lhs < rhs);
+	};
+
+    template <class T1, class T2>
+	ft::pair<T1,T2> make_pair(T1 x, T2 y)
+	{
+		return (ft::pair<T1, T2>(x, y));
+	};
+
+
 }
 
 #endif
