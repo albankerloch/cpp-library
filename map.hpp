ba81ead759ac19d0035ec4590f3fae87ef31ddf1
albankerloch <alban.kerloch@gmail.com>
2021-10-21 02:29:17 +0200

    save

--- map.hpp
+++ map.hpp
@@ -27,24 +27,35 @@ namespace ft
 	{
 		public:
 
-		typedef Key												key_type;
-		typedef T												mapped_type;
-		typedef ft::pair<const key_type, mapped_type>			value_type;
-		typedef Compare											key_compare;
-		class													value_compare;
-		typedef Alloc											allocator_type;
-		typedef typename allocator_type::reference				reference;
-		typedef typename allocator_type::const_reference		const_reference;
-		typedef typename allocator_type::pointer				pointer;
-		typedef typename allocator_type::const_pointer			const_pointer;
-		typedef ft::TreeNode<value_type>						node_type;
-		typedef node_type*										node_ptr;
-		typedef ptrdiff_t										difference_type;
-		typedef size_t											size_type;
-		typedef ft::MapIterator<value_type, node_type>			iterator;
-		typedef ft::MapIterator<const value_type, node_type>	const_iterator;
-		typedef ft::reverse_iterator<iterator>					reverse_iterator;
-		typedef ft::reverse_iterator<const_iterator>			const_reverse_iterator;
+			typedef Key												key_type;
+			typedef T												mapped_type;
+			typedef ft::pair<const key_type, mapped_type>			value_type;
+			typedef Compare											key_compare;
+			class													value_compare;
+			typedef Alloc											allocator_type;
+			typedef typename allocator_type::reference				reference;
+			typedef typename allocator_type::const_reference		const_reference;
+			typedef typename allocator_type::pointer				pointer;
+			typedef typename allocator_type::const_pointer			const_pointer;
+			typedef ft::TreeNode<value_type>						node_type;
+			typedef node_type*										node_ptr;
+			typedef ptrdiff_t										difference_type;
+			typedef size_t											size_type;
+			typedef ft::MapIterator<value_type, node_type>			iterator;
+			typedef ft::MapIterator<const value_type, node_type>	const_iterator;
+			typedef ft::reverse_iterator<iterator>					reverse_iterator;
+			typedef ft::reverse_iterator<const_iterator>			const_reverse_iterator;
+
+		private:
+
+			node_ptr				_data;
+			key_compare				_key_cmp;
+			allocator_type			_alloc;
+			size_type				_size;
+			const static size_type	_max_size;
+
+
+		public:
 
 	// ************************** Member functions ****************************** //
 
@@ -114,13 +125,7 @@ namespace ft
 
 	// ******************************* Non-public ******************************* //
 
-		protected:
 		private:
-		node_ptr				_data;
-		key_compare				_key_cmp;
-		allocator_type			_alloc;
-		size_type				_size;
-		const static size_type	_max_size;
 
 		template <class Ite>
 		void				_create_data_it(Ite first, Ite last);
