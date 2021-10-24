459411032859c691b29cde034b067306cc148760
albankerloch <alban.kerloch@gmail.com>
2021-10-24 17:02:33 +0200

    vector iterator

--- Random_Access_Iterator.hpp
+++ Random_Access_Iterator.hpp
@@ -10,112 +10,110 @@ namespace ft {
 	{
 		protected:
 
-		T									*m_value;
+			T	*m_value;
 
 		public:
 
-		typedef T																						value_type;
-		typedef typename ft::iterator<ft::random_access_iterator_tag, value_type>::difference_type		difference_type;
-		typedef typename ft::iterator<ft::random_access_iterator_tag, value_type>::iterator_category	iterator_category;
+			typedef T																						value_type;
+			typedef typename ft::iterator<ft::random_access_iterator_tag, value_type>::difference_type		difference_type;
+			typedef typename ft::iterator<ft::random_access_iterator_tag, value_type>::iterator_category	iterator_category;
 
 
-		RandomAccessIterator(void): m_value(NULL) 
-		{
-		};
+			RandomAccessIterator(void): m_value(NULL) 
+			{
+			};
 
-		RandomAccessIterator(T *src) : m_value(src)
-		{
-		};
+			RandomAccessIterator(T *src) : m_value(src)
+			{
+			};
 
-		RandomAccessIterator(const RandomAccessIterator &src)
-		{
-			*this = src;
-		};
+			RandomAccessIterator(const RandomAccessIterator &src)
+			{
+				*this = src;
+			};
 
-		virtual ~RandomAccessIterator(void)
-		{
-		};
+			virtual ~RandomAccessIterator(void)
+			{
+			};
 
-		RandomAccessIterator	&operator=(RandomAccessIterator const &rhs)
-		{
-			if (this == &rhs)
+			RandomAccessIterator	&operator=(RandomAccessIterator const &rhs)
+			{
+				if (this == &rhs)
+					return (*this);
+				this->m_value = rhs.m_value;
 				return (*this);
-			this->m_value = rhs.m_value;
-			return (*this);
-		};
+			};
 
-		bool	operator==(const RandomAccessIterator &rhs) const
-		{
-			return (this->m_value == rhs.m_value);
-		};
+			bool	operator==(const RandomAccessIterator &rhs) const
+			{
+				return (this->m_value == rhs.m_value);
+			};
 
-		bool	operator!=(const RandomAccessIterator &rhs) const
-		{
-			return (this->m_value != rhs.m_value);
-		};
+			bool	operator!=(const RandomAccessIterator &rhs) const
+			{
+				return (this->m_value != rhs.m_value);
+			};
 
-		bool	operator<(const RandomAccessIterator &rhs) const
-		{
-			return (this->m_value < rhs.m_value);
-		};
+			bool	operator<(const RandomAccessIterator &rhs) const
+			{
+				return (this->m_value < rhs.m_value);
+			};
 
-		bool	operator<=(const RandomAccessIterator &rhs) const
-		{
-			return (this->m_value <= rhs.m_value);
-		};
+			bool	operator<=(const RandomAccessIterator &rhs) const
+			{
+				return (this->m_value <= rhs.m_value);
+			};
 
-		bool	operator>(const RandomAccessIterator &rhs) const
-		{
-			return (this->m_value > rhs.m_value);
-		};
+			bool	operator>(const RandomAccessIterator &rhs) const
+			{
+				return (this->m_value > rhs.m_value);
+			};
 
-		bool	operator>=(const RandomAccessIterator &rhs) const
-		{
-			return (this->m_value >= rhs.m_value);
-		};
+			bool	operator>=(const RandomAccessIterator &rhs) const
+			{
+				return (this->m_value >= rhs.m_value);
+			};
 
-		RandomAccessIterator<T>	&operator++(void)
-		{
-			++this->m_value;
-			return (*this);
-		};
+			RandomAccessIterator<T>	&operator++(void)
+			{
+				++this->m_value;
+				return (*this);
+			};
 
-		RandomAccessIterator<T>	operator++(int)
-		{
-			RandomAccessIterator<T>	tmp(*this);
-			++this->m_value;
-			return (tmp);
-		};
+			RandomAccessIterator<T>	operator++(int)
+			{
+				RandomAccessIterator<T>	tmp(*this);
+				++this->m_value;
+				return (tmp);
+			};
 
-		RandomAccessIterator<T>	&operator--(void)
-		{
-			--this->m_value;
-			return (*this);
-		};
+			RandomAccessIterator<T>	&operator--(void)
+			{
+				--this->m_value;
+				return (*this);
+			};
 
-		RandomAccessIterator<T>	operator--(int)
-		{
-			RandomAccessIterator<T>	tmp(*this);
-			--this->m_value;
-			return (tmp);
-		};
+			RandomAccessIterator<T>	operator--(int)
+			{
+				RandomAccessIterator<T>	tmp(*this);
+				--this->m_value;
+				return (tmp);
+			};
 
-		difference_type				operator-(const RandomAccessIterator &rhs) const
-		{
-			return (this->m_value - rhs.m_value);
-		};
+			difference_type				operator-(const RandomAccessIterator &rhs) const
+			{
+				return (this->m_value - rhs.m_value);
+			};
 
-		RandomAccessIterator<T>			operator+(difference_type n) const
-		{
-			return (RandomAccessIterator(this->m_value + n));
-		};
+			RandomAccessIterator<T>			operator+(difference_type n) const
+			{
+				return (RandomAccessIterator(this->m_value + n));
+			};
 
-		RandomAccessIterator<T>			operator-(difference_type n) const
-		{
-			return (RandomAccessIterator(this->m_value - n));
-		};
-
-		friend RandomAccessIterator<T>	operator+(difference_type n, const RandomAccessIterator &rhs);
+			RandomAccessIterator<T>			operator-(difference_type n) const
+			{
+				return (RandomAccessIterator(this->m_value - n));
+			};
 
 	};
 
